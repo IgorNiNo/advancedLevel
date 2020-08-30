2f445763bf18f90e427e2fc4185adb278edd3c16
IgorNiNo <vis_74@mail.ru>
2020-08-30 22:10:01 +0300

    developLesson4

---
+++ src/lesson4/Controller.java
@@ -0,0 +1,20 @@
+package lesson4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextField textField;
+
+    public void sendText(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.requestFocus();
+        textField.clear();
+    }
+}
